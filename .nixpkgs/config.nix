b518658ded3dacd636ab1354273d1022f9a3e885
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-09 18:58:32 +0000

    update notes

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,15 +1,16 @@
 # TODO(akavel): why doesn't this file require args specification in this line?
 #  ANSWER: because it's just a nix set, not a func. [correct?]
 {
-  # TODO(akavel): can I specify the 'pkgs' below in {} curly brackets, or not?
-  #  ANSWER: I think not, because e.g. {x} would be "pattern-matching", i.e. would expect pkgs to be a set containing 'x'.
+  # TODO(akavel): how come the `with pkgs_` has a `pkgs` inside? is this some
+  # fixpoint magic?
   packageOverrides = pkgs_: with pkgs_; {
 
-    # "Declarative user profile/config". Installed/updated with `nix-env -i all`.
-    # NOTE(akavel): It's apparently faster to use `nix-env -iA nixos.all` instead.
+    # "Declarative user profile/config". Install/update with `nix-env -i all`.
+    # NOTE(akavel): Aparently `nix-env -iA nixos.all` is a faster variant.
     all = with pkgs; buildEnv {
       # Make it easy to install with `nix-env -i all` [I believe]
-      # TODO(akavel): verify that below line is required [or advised] for `nix-env -i all`
+      # TODO(akavel): verify that below line is required [or advised] for
+      # `nix-env -i all`
       name = "all";
 
       paths = [
