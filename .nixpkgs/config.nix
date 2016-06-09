61c625715450a83a9468ca805e508cf114f78212
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-09 21:59:47 +0000

    some notes about Nix language

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -78,5 +78,17 @@
   fixpoint result). Note that `with pkgs` in all-packages.nix still allows
   to access pkgs by name.
 
+## Various notes about Nix language
+
+- `let` is recursive and lazy: `let x = x; in x` gives error about infinite
+  recursion; `let x = x; in 5` just evaluates to `5`.
+- `with foo; ...` still keeps `foo` visible inside the `...` code.
+- `{ inherit (foo.bar) baz; }` is shortcut for `{ baz = foo.bar.baz; }`.
+- `import ./foo/bar.nix arg1 arg2` loads code from ./foo/bar.nix and then
+  calls it as a function with arguments `arg1` and `arg2`.
+- `foo.bar or baz` returns `foo.bar` if `foo` has attribute `bar`, or
+  expression `baz` otherwise. In other words, `baz` is a "default value" if
+  `.bar` is missing.
+
 ***** */
 
