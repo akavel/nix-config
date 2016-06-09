1213bacdddb4e6577ee1633f086b5bfcc37639d9
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-09 21:24:08 +0000

    explanation of origin of pkgs in packageOverrides

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -28,3 +28,53 @@
     };
   };
 }
+
+/* **** Notes: *****
+
+## Where does 'pkgs' come from in 'packageOverrides' in ~/.nixpkgs/config.nix?
+
+-> start explanation from pkgs/top-level/default.nix
+  (this is what imports ~/.nixpkgs/config.nix)
+
+-> explain lib.extends (a la inheritance) and lib.fix'
+  - lib.fix' (
+      lib.extends grandchild (
+        lib.extends child (
+          lib.extends parent (
+            empty))))
+    where:
+      empty = self: {};
+      grandpa = final: myparent: { ..... };
+      parent  = final: myparent: { ..... };
+      junior  = final: myparent: { ..... };
+    Each func in the "family" can take a "default" value from past/"ancestor"
+    (via `myparent.someField`), or a "future" value which will take into
+    account all calculations done by all children/"descendants" (via
+    `final.someField`), and pass it down to immediate successor. For example:
+
+        let
+          lib = import ./nixpkgs/lib/trivial.nix;
+          grandpa = final: myparent:
+            { name="Aaron";
+              aaronsBook="my grandchild will be "+final.name; };
+          parent = final: myparent: rec
+            { name="Bill";
+              billsBook="I'm "+name+", my dad is "+myparent.name; };
+          junior = final: myparent: rec
+            { name="Chad";
+              chadsBook="I'm "+name+", my dad is "+myparent.name; };
+          empty = self: {};
+        in
+          lib.fix' (
+            lib.extends junior (
+              lib.extends parent (
+                lib.extends grandpa (empty))))
+
+-> then func `allPackages` imports `./all-packages.nix`, which defines `pkgs`.
+  It's value is actually passed as an argument to the import, and nonetheless
+  resolves to the final ("future") set of calculated packages (the final
+  fixpoint result). Note that `with pkgs` in all-packages.nix still allows
+  to access pkgs by name.
+
+***** */
+
