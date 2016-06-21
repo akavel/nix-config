7aa4fd04d130fc2f0bee5066eb94c64e701035fa
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-21 22:26:31 +0000

    lib-vamos.nix: serious refactoring for readability

--- .nixpkgs/lib-vamos.nix
+++ .nixpkgs/lib-vamos.nix
@@ -7,45 +7,43 @@ let
   */
   vamos = pluginsList: {
     vam = {
-      pluginDictionaries = map toVamPD (builtins.filter needsVam pluginsList);
-      knownPlugins = vimPlugins // {
-        "vim-addon-manager" = vimPlugins."vim-addon-manager";
-      } // (builtins.listToAttrs (map toVamKP (onlyGitHub pluginsList)));
+      pluginDictionaries = pluginsAttr "pluginDictionaries" pluginsList;
+      knownPlugins =
+        vimPlugins // (builtins.listToAttrs (pluginsAttr "knownPlugins" pluginsList));
     };
-    customRC = builtins.foldl' (x: y: x+"\n"+y) "" (
-      map (plugin: plugin.config or "") pluginsList
-    );
+    customRC = builtins.foldl' concat "" (pluginsAttr "config" pluginsList);
   };
 
-  needsVam = plugin: plugin ? fromGitHub || plugin ? fromVam;
+  pluginsAttr = attr: pluginsList:
+    listAttr attr (map toVamWithConfig pluginsList);
+  toVamWithConfig = plugin: (toVam plugin) // (
+    if plugin ? config then {
+      config = plugin.config;
+    } else {});
+  toVam = plugin:
+    if plugin ? fromGitHub then {
+      pluginDictionaries = { name = baseNameOf plugin.fromGitHub; };
+      knownPlugins = knownFromGitHub plugin;
+    } else if plugin ? fromVam then {
+      pluginDictionaries = { name = plugin.fromVam; };
+      knownPlugins = { name=plugin.fromVam; value=builtins.getAttr plugin.fromVam vimPlugins; };
+    } else {};
 
-  onlyGitHub = list: builtins.filter (p: p ? fromGitHub) list;
-  # TODO(akavel): slash ('/') in names is not allowed for vimUtils - but I'd like to have it ?
-  toVamName = plugin:
-    if plugin ? fromGitHub then
-      baseNameOf plugin.fromGitHub
-    else if plugin ? fromVam then
-      plugin.fromVam
-    else throw "vamos can't handle plugin $(toString plugin)";
-  toVamPD = plugin: { name = toVamName plugin; };
-  toVamKP = plugin:
-    if plugin ? fromGitHub then
-      knownFromGitHub plugin
-    else if plugin ? fromVam then
-      { name=plugin.fromVam; value=builtins.getAttr plugin.fromVam vimPlugins; }
-    else throw "vamos can't handle plugin $(toString plugin)";
+  listAttr = attr: list:
+    map (builtins.getAttr attr) (builtins.filter (builtins.hasAttr attr) list);
+  concat = x: y:
+    x + "\n" + y;
+
   knownFromGitHub = plugin:
     let
-      name = toVamName plugin;
-      len = builtins.stringLength;
       repo = baseNameOf plugin.fromGitHub;
-      owner = builtins.substring 0 (len plugin.fromGitHub - len repo - 1) plugin.fromGitHub;
+      owner = builtins.substring 0 (strlen plugin.fromGitHub - strlen repo - 1) plugin.fromGitHub;
+      strlen = builtins.stringLength;
     in {
-      inherit name;
+      name = repo;
       value = vimUtils.buildVimPluginFrom2Nix {
-        inherit name;
+        name = repo;
         src = fetchFromGitHub {
-          # owner, repo, rev, sha256
           inherit (plugin) rev sha256;
           inherit owner repo;
         };
