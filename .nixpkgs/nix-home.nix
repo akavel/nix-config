53b0468efb145f403dc63a24243568f5263a757e
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-29 22:52:04 +0000

    nix-home: fixmes & todos

--- .nixpkgs/nix-home.nix
+++ .nixpkgs/nix-home.nix
@@ -11,9 +11,11 @@ let
     version = "0.0.1-2016.06.29";
     builder = writeText "builder.sh" ''
       source $stdenv/setup
+      # FIXME(akavel): verify that all files listed in 'dirs' can be created in $HOME/$d/... - i.e. they're missing or are appropriate links
       install ${nixHomeScript} -D $out/bin/nix-home
     '';
   };
+  # FIXME(akavel): create actual files listed in 'dirs'
   nixHomeScript = writeScript "nix-home" ''
     #! ${bash}/bin/bash
     echo `which nix-env` "$@"
@@ -21,8 +23,10 @@ let
     # Cleanup old links
     for d in ${ bashList (builtins.attrNames dirs) }; do
       echo "$d"
+      # FIXME(akavel): delete all links in $HOME/$d dir which point to corresponding path in $HOME/.nix-profile/${storePath}
     done
 
+    # TODO(akavel): try to put the files in /nix/store/.../, not in ~/.nix-profile/...
     for f in $(find "$HOME/.nix-profile/${storePath}" 2>/dev/null); do
       echo "$f"
     done
