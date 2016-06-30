32ecbbe1b6b82cbbad1693f2bfa0bd0aefdc2a05
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-30 23:12:42 +0000

    nix-home: attempt at adding the real script

--- .nixpkgs/nix-home.nix
+++ .nixpkgs/nix-home.nix
@@ -8,7 +8,14 @@
 
 # TODO(akavel): could we just return a list of writeTextFile derivations?
 let
-  nixHome = linksTree "homedir" (lib.attrValues (lib.mapAttrs mkStoreEntry files));
+  nixHome = linksTree "homedir" (lib.attrValues (lib.mapAttrs mkStoreEntry files // { 
+    # TODO(akavel): make ~/.nix-profile non-hardcoded somehow?
+    # TODO(akavel): make sure to disallow '.nix-profile' on the list of files; similar maybe for e.g. .git, .nix-defexpr, .nixpkgs, ...
+    "bin/nix-home" = writeScriptBin "nix-home" ''
+      #! ${bash}/bin/bash
+      ${./nix-home.sh} ~/.nix-profile/${esc storePath} ~
+    '';
+  }));
   # linksTree is similar to linkFarm, but can create nested links
   # TODO(akavel): better doc
   linksTree = name: entries: runCommand name {} ("mkdir -p $out; cd $out;\n" +
---
+++ .nixpkgs/nix-home.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+src="${1:-~/.nix-profile/etc/nix-home}"
+dst="${2:-~}"
+
+function err {
+    echo "$(basename "$0"): $@" >&2
+}
+function die {
+    err "$@"
+    exit 1
+}
+# tempfile prints path of a newly created temporary file
+function tempfile {
+    mktemp --suffix=".$$.$(basename "$0")"
+}
+# unmkdir removes empty dirs from $root down to $path
+function unmkdir {
+    local root="$1"
+    local path="$2"
+    while [ ${#path} -gt ${#root} ]; do
+        rmdir "$path" || return
+        path="$(dirname "$path")"
+    done
+}
+# subtree prints relative paths of all files in $root tree, separated by NUL byte and sorted
+function subtree {
+    local root="$1"
+    find "$root/" -type f -printf '%P\0' |
+        sort -z
+}
+
+# Remember current state of $src in $oldsrc
+# TODO(akavel): maybe try pinning old nix profile instead?
+oldsrc="$( tempfile )"
+trap "rm '$oldsrc'" EXIT
+subtree "$src" > "$oldsrc"
+
+# TODO(akavel): For safety, verify $dst paths to be potentially removed are all correct links
+
+if [ $# -eq 0 ]; then
+  nix-env -iA nixos.home
+else
+  nix-env "$@"
+fi
+
+# Create links to added files
+comm -z -13 "$oldsrc" <( subtree "$src" ) |
+    while IFS= read -r -d '' path; do
+        printf "add: %q\n" $path
+        mkdir -p "$(dirname "$dst/$path")"
+        ln -s -r "$src/$path" "$dst/$path"
+    done
+# Remove links to removed files
+comm -z -23 "$oldsrc" <( subtree "$src" ) |
+    while IFS= read -r -d '' path; do
+        printf "remove: %q\n" $path
+        if [ ! -L "$dst/$path" ]; then
+            err "$dst/$path: is not a symbolic link, refusing to remove"
+            continue
+        fi
+        rm "$dst/$path"
+        unmkdir "$dst" "$(dirname "$dst/$path")"
+    done
+
+
