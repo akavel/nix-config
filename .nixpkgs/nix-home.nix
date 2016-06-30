87fb297d292621b95af5972b8577be2d97717fb9
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-30 23:21:31 +0000

    nix-home: create wrapper in bin/

--- .nixpkgs/nix-home.nix
+++ .nixpkgs/nix-home.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, writeTextFile, runCommand
+{ stdenv, lib, writeTextFile, writeScript, runCommand
 , bash
 
 # TODO(akavel): use mkOption etc.? how?
@@ -8,14 +8,15 @@
 
 # TODO(akavel): could we just return a list of writeTextFile derivations?
 let
-  nixHome = linksTree "homedir" (lib.attrValues (lib.mapAttrs mkStoreEntry files // { 
+  nixHome = linksTree "homedir" (lib.attrValues (lib.mapAttrs mkStoreEntry files) ++ [{
     # TODO(akavel): make ~/.nix-profile non-hardcoded somehow?
     # TODO(akavel): make sure to disallow '.nix-profile' on the list of files; similar maybe for e.g. .git, .nix-defexpr, .nixpkgs, ...
-    "bin/nix-home" = writeScriptBin "nix-home" ''
+    name = "bin/nix-home";
+    path = writeScript "nix-home-wrapper" ''
       #! ${bash}/bin/bash
       ${./nix-home.sh} ~/.nix-profile/${esc storePath} ~
     '';
-  }));
+  }]);
   # linksTree is similar to linkFarm, but can create nested links
   # TODO(akavel): better doc
   linksTree = name: entries: runCommand name {} ("mkdir -p $out; cd $out;\n" +
