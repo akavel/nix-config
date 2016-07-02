13478501cc2f6f4509e921ae0aaf1bd16789bb07
Mateusz Czaplinski <czapkofan@gmail.com>
2016-07-02 22:02:52 +0000

    config: WIP try to enable awesome WM via ~/.xession

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -56,6 +56,7 @@
 
     nix-home = callPackage ./nix-home.nix {
       files = {
+        ".xsession" = "exec awesome";
         #"/fooFile" = "hello world";
         #"/sub/barfile" = "hello bar world";
       };
