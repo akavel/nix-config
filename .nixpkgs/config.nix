6f5169a212ab67a3731529e9522384ac68367d0c
Mateusz Czaplinski <czapkofan@gmail.com>
2016-07-02 23:15:46 +0000

    config: use default config for "awesome" window-manager

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -57,6 +57,8 @@
     nix-home = callPackage ./nix-home.nix {
       files = {
         #".xsession" = writeScript "my-.xession" "exec awesome";
+        # For initial learning, copy default config for "awesome" window-manager
+        ".config/awesome/rc.lua" = runCommand "my-awesome-rc" {} "ln -s ${awesome}/etc/xdg/awesome/rc.lua $out";
         # NOTE: bash reads the following config files in following cases (according to `man bash`):
         #  * ~/.bash_profile or ~/.bash_login or ~/.profile -- when run as a login shell (implied interactive)
         #  * ~/.bashrc -- interactive, but non-login shell [notably in SSH sessions, and I think in XWindows sessions?]
