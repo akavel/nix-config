95b56f417718e3f77a2e8c339056bad3bc7dba50
Mateusz Czapliński <mateusz.czaplinski@zpas-lab.pl>
2017-06-14 13:24:35 +0200

    conf-zpaslab.nix: initial testing of nix-home

--- .nixpkgs/conf-zpaslab.nix
+++ .nixpkgs/conf-zpaslab.nix
@@ -1,3 +1,18 @@
 {pkgs, ...}:
 {
+  packageOverrides = defaultPkgs: with defaultPkgs; {
+    # Declarative user profile/config. Install/update with `nix-env -i home`.
+    home = with pkgs; buildEnv {
+      name = "home";
+      paths = [
+        nix-home
+      ];
+    };
+
+    nix-home = callPackage ./nix-home.nix {
+      files = {
+        "sample-home-test" = "sample-test";
+      };
+    };
+  };
 }
