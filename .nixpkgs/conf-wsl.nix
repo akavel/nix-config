8cff8eebb7cab072b1d321b4e84f3db75088a5b9
Mateusz Czapliński <czapkofan@gmail.com>
2017-06-27 22:56:03 +0200

    conf-wsl.nix: first test on Windows Subsystem for Linux

---
+++ .nixpkgs/conf-wsl.nix
@@ -0,0 +1,23 @@
+{pkgs, ...}:
+{
+  packageOverrides = defaultPkgs: with defaultPkgs; {
+    # Declarative user profile/config. Install/update with `nix-env -i home`.
+    home = with pkgs; buildEnv {
+      name = "home";
+      paths = [
+        nix-home
+      ];
+    };
+
+    nix-home = callPackage ./nix-home.nix {
+      files = {
+        # bash: case-insensitive TAB-completion
+        ".inputrc" = "set completion-ignore-case On";
+        #"sample-home-test" = "sample-test";
+        # ack: nice navigation if many results; keep colors enabled
+        ".ackrc" = "--pager=less -R";
+      };
+    };
+  };
+}
+
