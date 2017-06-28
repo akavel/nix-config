1de2ae8ff2fe7b436a06f9a6c85dad9704df0818
Mateusz Czapliński <mateusz.czaplinski@zpas-lab.pl>
2017-06-28 16:34:27 +0200

    conf-zpaslab: start adding customized vim

--- .nixpkgs/conf-zpaslab.nix
+++ .nixpkgs/conf-zpaslab.nix
@@ -5,10 +5,13 @@
     home = with pkgs; buildEnv {
       name = "home";
       paths = [
-        nix-home
+        nix-home  # see below
+        #neovim    # see below
+        xim       # see below
       ];
     };
 
+    # nix-home tool - for managing $HOME via Nix (wrapper for nix-env)
     nix-home = callPackage ./nix-home {
       files = {
         # bash: case-insensitive TAB-completion
@@ -18,5 +21,32 @@
         ".ackrc" = "--pager=less -R";
       };
     };
+
+    # NeoVim editor + customized config
+    #neovim = let
+      #neovim = vimUtils.vimWithRC {
+      #in neovim;
+
+    # Vim with customizations, named "xim" for testing
+    # TODO(akavel): disable python in vim, maybe
+    xim =
+      let
+        theXim = vimUtils.vimWithRC {
+          name = "xim";
+          vimExecutable = "${defaultPkgs.vim}/bin/vim";
+          vimrcFile = vimUtils.vimrcFile myVimrc;
+        };
+        vamos = callPackage ./lib-vamos.nix {};
+        myVimrc = vamos [
+          # Go language support for vim
+          { fromVam="vim-go"; }
+          # Smart and quick file opening with Ctrl-P, by fuzzy path match, like in SublimeText
+          { fromVam="ctrlp"; config=''
+              let g:ctrlp_extensions = ['mixed', 'line', 'buffertag', 'tag']
+              let g:ctrlp_custom_ignore = '\v\.pyc''$'
+            ''; }
+        ];
+      in theXim;
+
   };
 }
