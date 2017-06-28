eca569bffb65333a035dc049c1ad0afec5275175
Mateusz Czapliński <mateusz.czaplinski@zpas-lab.pl>
2017-06-28 17:51:43 +0200

    conf-zpaslab: more plugins, upto choosewin

--- .nixpkgs/conf-zpaslab.nix
+++ .nixpkgs/conf-zpaslab.nix
@@ -60,6 +60,7 @@
           #               'terryma/vim-multiple-cursors' is fairly limited, try some alternative.
           # Code snippets for vim.
           { fromVam="snipmate"; }
+          # TODO(akavel): try using fromVam="neocomplete";
           { fromGitHub="Shougo/neocomplete";
             rev = "7b2b1cf5ba60cf17f8d8c3535c21a51b32c1a52b";
             sha256 = "1f1qzk9wicxdpz1w4dlc99zlbq2slkva94ajajsb49m7vnyhh0wd";
@@ -79,7 +80,42 @@
             ''; }
           { fromGitHub="Shougo/echodoc.vim";
             rev = "ef0a0cbe6c4cdb60eb2a1cbcee5bde11ce2c6dce";
-            sha256 = "0vdfjyprnn59brkf0vnyq9ksh03labwyq4vyvsihs4zhing2jbwy"; }
+            sha256 = "0vdfjyprnn59brkf0vnyq9ksh03labwyq4vyvsihs4zhing2jbwy"; 
+            config = ''
+              let g:echodoc_enable_at_startup = 1
+              set cmdheight=2
+            ''; }
+          # TODO(akavel): find out which snippets plugin I'm actually using
+          # FIXME(akavel): fix the snippets directory to be configured via Nix
+          { fromVam="neosnippet";
+            config = ''
+              let g:neosnippet#disable_runtime_snippets = {'_': 1}
+              let g:neosnippet#enable_snipmate_compatibility = 1
+              let g:neosnippet#snippets_directory = '~/.vim/snippets'
+              imap <C-k>     <Plug>(neosnippet_expand_or_jump)
+              smap <C-k>     <Plug>(neosnippet_expand_or_jump)
+              xmap <C-k>     <Plug>(neosnippet_expand_target)
+            ''; }
+          # Git
+          { fromVam="fugitive"; }
+          { fromGitHub="tpope/vim-unimpaired";
+            rev = "3548479cc154154cbdbb6737ca08a47937cc7113";
+            sha256 = "0p2siip5xf20mg4z1z3m4mp90m026ww3cnkk1n9rc02xhp5xvpsg"; }
+          { fromVam="surround"; }
+          { fromVam="commentary"; }
+          { fromVam="supertab"; }
+          { fromVam="vim-repeat"; }
+          # Increase/decrease selection with +/_
+          { fromVam="vim-expand-region"; }
+          # Choose window (split) by number
+          { fromGitHub="t9md/vim-choosewin";
+            rev = "3e3ab81364ab1f772538c0250c0965f35f986e20";
+            sha256 = "00qakw7701lxmc7nvqf9c7inp7nld21491bvcm4s93z7f5l048vx"; 
+            config = ''
+              nmap - <Plug>(choosewin)
+              " let g:choosewin_overlay_enable = 1
+              let g:choosewin_overlay_enable = 0
+            ''; }
         ];
       in theXim;
 
