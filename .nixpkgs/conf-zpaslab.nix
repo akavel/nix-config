e4307ddc7e66bbb2a283deb06aacae6fbf7292f0
Mateusz Czapliński <mateusz.czaplinski@zpas-lab.pl>
2017-06-28 17:32:21 +0200

    conf-zpaslab: more plugins, incl. neocomplete

--- .nixpkgs/conf-zpaslab.nix
+++ .nixpkgs/conf-zpaslab.nix
@@ -46,13 +46,43 @@
         vamos = callPackage ./lib-vamos.nix {};
         # Note: see <nixpkgs>/pkgs/misc/vim-plugins/default.nix for names to use with fromVam
         myVimrc = vamos [
+          { fromGitHub="garyburd/go-explorer";
+            rev = "c22eec647bca26f61578d27a152141374cbe2854";
+            sha256 = "1rpj8y0jdpa8qpkk4hvxjfk16lk6j2b89y6bh4fdkmw3kbd1gz40"; }
           # Go language support for vim
           { fromVam="vim-go"; }
           # Smart and quick file opening with Ctrl-P, by fuzzy path match, like in SublimeText
-          { fromVam="ctrlp"; config=''
+          { fromVam="ctrlp";
+            config=''
               let g:ctrlp_extensions = ['mixed', 'line', 'buffertag', 'tag']
               let g:ctrlp_custom_ignore = '\v\.pyc''$'
             ''; }
+          # Ack is a better featured alternative to grep.
+          { fromVam="ack-vim"; }
+          # TODO(akavel): Attempt at multiple cursors like in SublimeText.
+          #               'terryma/vim-multiple-cursors' is fairly limited, try some alternative.
+          # Code snippets for vim.
+          { fromVam="snipmate"; }
+          { fromGitHub="Shougo/neocomplete";
+            rev = "7b2b1cf5ba60cf17f8d8c3535c21a51b32c1a52b";
+            sha256 = "1f1qzk9wicxdpz1w4dlc99zlbq2slkva94ajajsb49m7vnyhh0wd";
+            config = ''
+              let g:neocomplete#data_directory = "/tmp/neocomplete-swap"
+              " Use neocomplete.
+              let g:neocomplete#enable_at_startup = 1
+              " Enable heavy omni completion.
+              if !exists('g:neocomplete#sources#omni#input_patterns')
+                  let g:neocomplete#sources#omni#input_patterns = {}
+              endif
+              " golang fix
+              let g:neocomplete#sources#omni#input_patterns.go = '[^.[:digit:] *\t]\.\w*'
+              let g:neocomplete#enable_ignore_case = 1
+              let g:neocomplete#enable_smart_case = 1
+              let g:neocomplete#enable_auto_select = 1
+            ''; }
+          { fromGitHub="Shougo/echodoc.vim";
+            rev = "ef0a0cbe6c4cdb60eb2a1cbcee5bde11ce2c6dce";
+            sha256 = "0vdfjyprnn59brkf0vnyq9ksh03labwyq4vyvsihs4zhing2jbwy"; }
         ];
       in theXim;
 
